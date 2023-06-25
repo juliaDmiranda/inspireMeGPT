d607348cdb8a9ed91ecceba510b36cd59a9504e5
Julia Miranda Rodrigues <juliamr@id.uff.br>
2023-06-24 23:05:26 -0300

    Remove app login
    Put login a signup in the same app named accounts

--- django_project/login/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def loginPageView(request):
-    return render(request, "login.html")
--- django_project/signup/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def signupPageView(request):
-    return render(request, "signup.html")
