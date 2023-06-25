84c2750d482326a9402714d0d2be7b2bc34a51a0
Julia Miranda Rodrigues <juliamr@id.uff.br>
2023-06-24 23:06:23 -0300

    chat app

--- django_project/chat/models.py
+++ django_project/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ChatRoom(models.Model):
+    title = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Message(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Chat Room: {self.chat_room}, Sender: {self.sender.username}, Message: {self.message}"
