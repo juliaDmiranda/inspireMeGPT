937a9343e2d9b9f805047a02ba5ff26a813de9dd
Julia Miranda Rodrigues <juliamr@id.uff.br>
2023-06-27 11:40:00 -0300

    adicionar cache para evitar que usuário sem login tenha acesso ao chat

--- django_project/chat/views.py
+++ django_project/chat/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 # Create your views here.
-@login_required
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url='accounts:login')
 def homePageView(request):
     return render(request, "home.html")
 
