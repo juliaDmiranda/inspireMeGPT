a466fb6537c679dc6598aaa01c5056412e6a4cf7
Julia Miranda Rodrigues <juliamr@id.uff.br>
2023-06-27 11:42:33 -0300

    muda de posição

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LoginView
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("chat/", include("chat.urls")),  
     path("", include("accounts.urls")),  
+    path("chat/", include("chat.urls")),  
+    # path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),
     # path("signup/", include("signup.urls")),  
 ]
 if settings.DEBUG:
